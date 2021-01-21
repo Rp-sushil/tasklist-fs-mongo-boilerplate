18c7a2d27e1f0257aa40a3b9f6530d58abd143a5
ChetanKarwa <46815976+ChetanKarwa@users.noreply.github.com>
2021-01-21 12:23:12 +0530

    Update connector.js

--- src/connector.js
+++ src/connector.js
@@ -1,11 +1,11 @@
 const mongodb = require('mongodb');
 const mongoose = require('mongoose');
-let {todoSchema} = require('./schema');
+let {taskSchema} = require('./schema');
 
 const uri = ""; 
 
 mongoose.connect(uri, { useNewUrlParser: true, useUnifiedTopology: true ,useCreateIndex: true })
 .then(console.log("Success"))
 
-let collection_connection = mongoose.model('todo', todoSchema);
-exports.todo = collection_connection;
+let collection_connection = mongoose.model('task', taskSchema);
+exports.task = collection_connection;
